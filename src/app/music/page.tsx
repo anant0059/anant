f8ee685e0b4d15488e35c4305645fec7bd649eb6
anant0059 <2019uce0059@gmail.com>
2025-01-19 01:58:10 +0530

    Pv1: Added experiance

---
+++ src/app/music/page.tsx
@@ -0,0 +1,17 @@
+import Header from '../components/Header'
+
+export default function Music() {
+  return (
+    <main className="max-w-3xl mx-auto px-4 py-12 text-white">
+      <Header />
+      <div className="mb-12">
+        <h2 className="text-2xl font-mono text-gray-400 mb-8">Music</h2>
+        
+        <div className="space-y-8 text-gray-400 font-mono">
+          {/* Music content will go here */}
+          <p>Coming soon...</p>
+        </div>
+      </div>
+    </main>
+  )
+} 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,24 @@
 import Header from './components/Header'
 import Link from 'next/link'
 
-
 export default function Home() {
   return (
     <main className="max-w-3xl mx-auto px-4 py-12 text-white">
       <Header />
       <div className="space-y-8 font-mono">
         <p className="text-lg">
-          Hi! I'm Anant Kumar, a software engineer dedicated to crafting innovative solutions through software development, product design, and automation.  
+          Hi! I&apos;m Anant Kumar, a software engineer dedicated to crafting innovative solutions through software development, product development, and automation.
         </p>
 
         <p className="text-lg">
-          I’m a tech enthusiast who enjoys exploring new technologies, finding creative ways to solve problems, and continuously pushing the boundaries of what's possible.  
-          A music enthusiast who can get lost and high on melodies, I believe in maintaining balance, embracing inner peace, and fostering personal growth.  
-          Outside the tech world, I’m on a journey of exploring myself and discovering new perspectives through experiences and creativity.
+          I&apos;m a tech enthusiast passionate about exploring new technologies, finding creative ways to solve problems, and continuously pushing the boundaries of what&apos;s possible.
+          A lover of music who can get lost and high in its essence, I believe in maintaining balance, embracing inner peace, and fostering personal growth.
+          Outside the tech world, I&apos;m on a journey of self-discovery, gaining new perspectives through experiences and creative expression.
         </p>
         
         <div className="mt-12">
-        <h3 className="text-2xl text-gray-400 mb-4">
-            Here's my most recent posts or <Link href="/blog" className="underline hover:text-gray-300">read a random one!</Link>
+          <h3 className="text-2xl text-gray-400 mb-4">
+            Here&apos;s my most recent posts or <Link href="/blog" className="underline hover:text-gray-300">read a random one!</Link>
           </h3>
         </div>
       </div>
